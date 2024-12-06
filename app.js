cf51288b4c4e0788007a6f094bc0ca79d63ec0e4
Lee Morgan <lee@leemorgan.dev>
2024-12-06 10:05:03 -0500

    Create the server entry point.

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import compression from "compression";
+import cors from "cors";
+
+const app = express();
+global.cwd = import.meta.dirname;
+
+let mongoString = "mongodb://127.0.0.1/budget";
+if(process.env.NODE_ENV === "production"){
+    mongoString = `mongodb://website:${process.env.MONGODB_PASS}@127.0.0.1:27017/budget?authSource=admin`;
+}
+mongoose.connect(mongoString);
+
+app.use(compression());
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res)=>{res.sendFile(`${global.cwd}/index.html`)});
+
+if(process.env.NODE_ENV !== "production"){
+    app.listen(8000);
+}
+export default app;
