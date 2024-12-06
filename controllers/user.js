81de76d780c2162113a54496e41921f52b0ca005
Lee Morgan <lee@leemorgan.dev>
2024-12-06 12:29:28 -0500

    Create route to get user authorization token.

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,10 @@
 import User from "../models/user.js";
 
+import {CustomError} from "../CustomError.js";
 import validate from "../validation/user.js";
 import bcrypt from "bcrypt";
 import crypto from "crypto";
+import jwt from "jsonwebtoken";
 
 const create = async (req, res, next)=>{
     try{
@@ -13,6 +15,29 @@ const create = async (req, res, next)=>{
     }catch(e){
         next(e);
     }
+}
+
+const getToken = async (req, res, next)=>{
+    try{
+        const user = await getUserWithEmail(req.body.email.toLowerCase());
+        await checkPassword(user.password, req.body.password);
+        const token = createJWT(user);
+        res.json({token: token});
+    }catch(e){
+        next(e);
+    }
+}
+
+/*
+ Retrieve user from email address
+
+ @param {String} email - user email address
+ @return {User} User object
+ */
+const getUserWithEmail = async (email)=>{
+    const user = await User.findOne({email: email});
+    if(!user) throw new CustomError(400, "No user with this email address");
+    return user;
 }
 
 /*
@@ -61,6 +86,33 @@ const createNewToken = ()=>{
     return crypto.randomUUID();
 }
 
+/*
+ Check password
+ Throw error if no match
+
+ @param {string} hashedPass - Hashed password from DB
+ @param {string} pass - User entered password
+ */
+const checkPassword = async (hashedPass, pass)=>{
+    if(!await bcrypt.compare(pass, hashedPass)){
+        throw new CustomError(401, "Unauthorized");
+    }
+}
+
+/*
+ Create a new JWT for user authorization
+
+ @param {User} user - User object
+ @return {String} New JWT
+ */
+const createJWT = (user)=>{
+    return jwt.sign({
+        id: user._id,
+        token: user.token
+    }, process.env.JWT_SECRET);
+}
+
 export {
-    create
+    create,
+    getToken
 };
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,9 @@
 import {
-    create
+    create,
+    getToken
 } from "../controllers/user.js";
 
 export default (app)=>{
     app.post("/user", create);
+    app.post("/user/token", getToken);
 }
