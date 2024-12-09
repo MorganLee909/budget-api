842f3238e62d3104962bd32f33c5a607d64ebd3b
Lee Morgan <lee@leemorgan.dev>
2024-12-09 14:50:15 -0500

    Create outline for 'create account' route.

--- controllers/account.js
+++ controllers/account.js
@@ -1,10 +1,14 @@
 import Account from "../models/account.js";
 
 import {CustomError} from "../CustomError.js";
+import validate from "../validation/account.js";
 
 const create = async (req, res, next)=>{
     try{
-        
+        validate(req.body);
+        const account = createAccount(req.body);
+        await account.save();
+        res.json(account);
     }catch(e){next(e)}
 }
 
