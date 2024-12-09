5302a5d9c3354b779566fb57f02ac7267932bc98
Lee Morgan <lee@leemorgan.dev>
2024-12-09 15:37:22 -0500

    Create route to get accounts of a user.

--- controllers/account.js
+++ controllers/account.js
@@ -6,17 +6,30 @@ import validate from "../validation/account.js";
 const create = async (req, res, next)=>{
     try{
         validate(req.body);
-        const account = createAccount(req.body);
+        const account = createAccount(req.body.name, req.body.balance);
         res.locals.user.accounts.push(account._id);
-        await Promise.all(account.save(), res.locals.user.save());
+        await Promise.all([account.save(), res.locals.user.save()]);
         res.json(account);
     }catch(e){next(e)}
 }
 
-const createAccount = (data)=>{
+const getAccounts = async (req, res, next)=>{
+    try{
+        const accounts = await Account.find({_id: res.locals.user.accounts});
+        res.json(accounts);
+    }catch(e){next(e)}
+}
+
+/*
+ Create a new Account object
+
+ @param {String} name - Name for the account
+ @param {Number} balance - Current balance on the account
+ */
+const createAccount = (name, balance)=>{
     return new Account({
-        name: data.name,
-        balance: data.balance,
+        name: name,
+        balance: balance,
         income: [],
         bills: [],
         allowances: []
@@ -24,5 +37,6 @@ const createAccount = (data)=>{
 }
 
 export {
-    create
+    create,
+    getAccounts
 }
--- routes/account.js
+++ routes/account.js
@@ -1,8 +1,10 @@
 import {userAuth} from "../auth.js";
 import {
-    create
+    create,
+    getAccounts
 } from "../controllers/account.js";
 
 export default (app)=>{
     app.post("/account", userAuth, create);
+    app.get("/account/user/:userId", userAuth, getAccounts);
 }
